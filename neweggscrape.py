313c5a232af260c7e4913a72e95b405d2cfec05e
Balaji R K <tombalu23@gmail.com>
2019-04-22 20:13:13 +0530

    COMMIT2

--- neweggscrape.py
+++ neweggscrape.py
@@ -9,7 +9,29 @@ page = requests.get(my_url)
 page_soup= bs4.BeautifulSoup(page.text, 'html.parser')
 #print(page_soup)
 
+
+#grabs each product
 containers=page_soup.find_all('div', {'class':'item-container'})
-print(containers[0])
+#print(containers[0])
+
+filename="graphics_cards.csv"
+f = open(filename, 'w')
+headers = "BRAND, PRODUCT_NAME, PRICE"
+f.write(headers+'\n')
 
 
+
+for container in containers:
+    itembrand = container.find('div', {'class':'item-branding'})
+    brand = itembrand.a.img["title"]
+    name=container.a.img['title']
+    info=container.find('div', {'class':'item-info'})
+
+    #    price=info.find('div', {'class':'item-action'}).ul.li.text.strip('\r')
+    price = info.find('div', {'class': 'item-action'}).ul.find('li', {'class':'price-current'}).text.strip()
+    price_float=(price[11:17])
+    print(brand + '     ' + name + '    ' + price[:])
+
+    f.write(brand + ',' + name.replace(',', '|') + ',' + price[:].replace('\u20b9',"").replace(',',"").replace('–',"").replace('\n',"").strip() + '\n')
+
+f.close()
